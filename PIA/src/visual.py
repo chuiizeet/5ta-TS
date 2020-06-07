2c824f1aa5d2ca6dadd3a226487c53e375be6950
David Gallegos V <dgallegosv94@gmail.com>
2020-06-07 05:55:19 -0500

    Update PIA

--- PIA/src/visual.py
+++ PIA/src/visual.py
@@ -1,133 +1,106 @@
 import sys
 import json
 import collections
+from collections import Counter
 import math
 
 
 class ZipfLaw:
 
-    def __init__(self, path=("dictionary.txt", "./data/wordcount.json")):
+    def __init__(self, path=("dictionary1.txt", "./data/wordcount.json")):
         # def __init__(self, path=("d2016.bin", "r")):
-        self.N = 0
-        self.p = {}
-        self.A = 0
+        self.TotalWords = 0
+        self.Frequencies = {}
+        self.fileSize = 0
 
         try:
             self.path = sys.argv[1]
         except IndexError:
             self.path = path[0]
 
-        try:
-            self.jsonPath = sys.argv[2]
-        except IndexError:
-            self.jsonPath = path[1]
-
         self.essay = open(self.path, 'r')
-
-        with open(self.path, 'r') as file:
-            self.A = 682869
-
-        self.wordCount = {}
-        self.wordSplit()
-        self.jsonifyDict()
+        data = self.essay.read()
+        wordList = data.split()
+        self.Frequencies = Counter(wordList)
+        print(self.Frequencies)
+        self.fileSize = data.__sizeof__()
+        self.TotalWords = len(wordList)
+        self.DictionaryVolume = len(self.Frequencies.keys())
         self.draw()
-
         self.essay.close()
 
-    def wordSplit(self):
-        wordList = self.essay.read().split()
-
-        self.N = len(wordList)
-        for word in wordList:
-            if word.isalpha():
-                if word in self.wordCount:
-                    self.wordCount[word.lower()] += 1
-                else:
-                    self.wordCount[word.lower()] = 1
-
-    def jsonifyDict(self):
-
-        self.sortedWordCount = collections.OrderedDict(reversed(sorted(self.wordCount.items(), key=lambda t: t[1])))
-
-        with open(self.jsonPath, 'w') as jsonData:
-            json.dump(self.sortedWordCount, jsonData, ensure_ascii=False)
-
     def draw(self, WordsLimit=150):
-        x = []
-        y = []
-        general = []
+        probabilities = {}
+        Ci = {}
+        self.Frequencies = {k: v for k, v in reversed(sorted(self.Frequencies.items(), key=lambda item: item[1]))}
 
-        for i in range(len(self.sortedWordCount)):
-            milista = []
-            x_val = list(self.sortedWordCount.items())[i][0]
-            y_val = list(self.sortedWordCount.items())[i][1]
-            x.append(x_val)
-            y.append(y_val)
-            milista.append(x_val)
-            milista.append(y_val)
-            general.append(milista)
+        for key in self.Frequencies.keys():
+            Ci.update({key: (self.Frequencies[key] / self.TotalWords)})
 
-        # Volumen del diccionario
-        D = len(x)
+        count = 1
+        for key in Ci:
+            probabilities.update({key: Ci[key] / count})
+            count += 1
+
         sumPi = 0
+        for key in probabilities.keys():
+            sumPi += probabilities[key] * (-math.log10(probabilities[key]))
 
-        for i in general:
-            self.p.update({i[0]: (i[1] / self.N) / (general.index(i) + 1)})
-
-        for key in self.p.keys():
-            sumPi += self.p[key] * (-math.log(self.p[key], 10))
-
-        sumFreq = 0
-        for i in range(1, D):
-            sumFreq += general[i][1] * (1 / i)
-
+        print("Probabilidades")
+        print(probabilities)
         print("Cantidad total de palabras en el diccionario:")
-        print(self.N)
+        print(self.TotalWords)
+        print("Longitud inicial del texto")
+        print(self.fileSize)
         print("Volumen del diccionario del Lenguaje Natural")
-        print(D)
-        print("Frecuencias:")
-        print(general)
-        print("Probabilidad de cada palabra (Pi):")
-        print(self.p)
-        print("Bajo la codificacion de Shanon B1 (Longitud del texto comprimido) es igual a:")
-        B1 = self.N * sumPi
-        print(round(B1, 4))
-        tmp = B1
-        print("Bajo la codificacion uniforme B1 (Longitud del texto comprimido) es igual a:")
-        B2 = self.N * math.log(D, 10)
-        print(round(B2, 4))
+        print(self.DictionaryVolume)
+        print("Bajo la codificacion de Shannon B1 (Longitud del texto comprimido) es igual a:")
+        B1 = self.TotalWords * sumPi
+        print(round(B1, 4), "bits")
+        print("Bajo la codificacion uniforme B2 (Longitud del texto comprimido) es igual a:")
+        B2 = self.TotalWords * math.log10(self.DictionaryVolume)
+        print(round(B2, 4), "bits")
+        B1final = 0
+        # for key in self.Frequencies.keys():
+        #     sum = 0
+        #     sum2 = 0
+        #     sumD = 0
+        #     for i in range(1, self.DictionaryVolume):
+        #         sumD += 1 / i
+        #         sum += ((self.Frequencies[key] / i) * (math.log(i, 10) - math.log(self.Frequencies[key] + 1)))
+        #         sum2 += ((math.log(i) / i) + (math.log(2) - math.log(self.Frequencies[key]) * sumD))
+        #         if B1final < (self.TotalWords * sum):
+        #             B1final = ((self.TotalWords * self.Frequencies[key]) / math.log(2)) * sum2
+        #             if B1final < (self.TotalWords / math.log(2) * math.log(self.DictionaryVolume + 1)) * (
+        #                     (math.pow(math.log(self.DictionaryVolume + 1), 2)) + -0.105 + (
+        #                     math.log(2) + math.log(math.log(self.DictionaryVolume + 1)) + 0.577) * (
+        #                             math.log(self.DictionaryVolume + 1) + 0.577)) and (
+        #                     (self.TotalWords / math.log(2) * math.log(self.DictionaryVolume + 1)) *
+        #                     ((math.pow(math.log(self.DictionaryVolume + 1), 2) / 2)
+        #                      + (-0.105) +
+        #                      (math.log(2) + math.log(2) * math.log(self.DictionaryVolume + 1) + (
+        #                              0.577 / math.log(self.DictionaryVolume + 1))) * (
+        #                              math.log(self.DictionaryVolume + 1) + 0.577))):
+        #                 B1final = ((self.TotalWords / (math.log(2) * math.log(self.DictionaryVolume + 1))) * (
+        #                         (math.pow(math.log(self.DictionaryVolume + 1), 2)) / 2) + 0.577 * (1 + math.log(2)) + (
+        #                                -0.105) + (
+        #                                    math.log(2) * math.log(self.DictionaryVolume + 1)) * (
+        #                                    math.log(self.DictionaryVolume + 1) + math.log(2)) + 0.577 * (
+        #                                    math.log(2) * math.log(self.DictionaryVolume + 1)) + (
+        #                                    math.pow(0.577, 2) / math.log(self.DictionaryVolume + 1)))
 
-        print("Sumatoria Bakulina(7) frecuencias:")
-        print(round(sumFreq, 4))
-
-        B1_n1 = []
-        for key in self.p.keys():
-            sum = 0
-            sum2 = 0
-            sumD = 0
-            for i in range(1, D):
-                sumD += 1 / i
-                sum += ((self.p[key] / i) * (math.log(i, 10) - math.log(self.p[key] + 1)))
-                sum2 += ((math.log(i) / i) + (math.log(2) - math.log(self.p[key]) * sumD))
-            if tmp < (self.N * sum):
-                tmp = ((self.N * self.p[key]) / math.log(2)) * sum2
-                if tmp < (self.N / math.log(2) * math.log(D + 1)) * (
-                        (math.pow(math.log(D + 1), 2)) + -0.105 + (
-                        math.log(2) + math.log(math.log(D + 1)) + 0.577) * (
-                                math.log(D + 1) + 0.577)) and ((self.N / math.log(2) * math.log(D + 1)) *
-                                                               ((math.pow(math.log(D + 1), 2) / 2)
-                                                                + (-0.105) +
-                                                                (math.log(2) + math.log(2) * math.log(D + 1) + (
-                                                                        0.577 / math.log(D + 1))) * (
-                                                                        math.log(D + 1) + 0.577))):
-                    tmp = ((self.N / (math.log(2) * math.log(D + 1))) * (
-                            (math.pow(math.log(D + 1), 2)) / 2) + 0.577 * (1 + math.log(2)) + (-0.105) + (
-                                   math.log(2) * math.log(D + 1)) * (math.log(D + 1) + math.log(2)) + 0.577 * (
-                                   math.log(2) * math.log(D + 1)) + (math.pow(0.577, 2) / math.log(D + 1)))
-
-            B1_n1.append(tmp)
-            tmp = B1
-        print(B1_n1)
+        n1 = B1 / self.fileSize
+        n2 = B2 / self.fileSize
+        bitsTotales = self.fileSize * 8
+        print("Validacion n1/n2 = B1/B2")
+        print(B1 / B2)
+        print(n1 / n2)
+        print("n1")
+        print(n1, "bits")
+        print("n2")
+        print(n2, "bits")
+        print((B1 / self.fileSize) * 100)
+        print((B2 / self.fileSize) * 100)
 
 
 if __name__ == '__main__':
--- PIA/zipf.py
+++ PIA/zipf.py
@@ -5,8 +5,6 @@ import src.visual as sv
 try:
     sv.ZipfLaw()
 except Exception as error:
-    print("'dictionary.txt' el archivo a leer no debe estar vacío, asegurese de agregar palabras al diccionario")
-    print("o")
     print(error)
 finally:
     sys.exit()
