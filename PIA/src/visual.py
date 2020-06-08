c9d3d2e28b0c1a7f3b0678b3ea5ae3d57b4d1aa1
David Gallegos V <dgallegosv94@gmail.com>
2020-06-08 02:22:11 -0500

    2.- Done

--- PIA/src/visual.py
+++ PIA/src/visual.py
@@ -7,8 +7,7 @@ import math
 
 class ZipfLaw:
 
-    def __init__(self, path=("dictionary1.txt", "./data/wordcount.json")):
-        # def __init__(self, path=("d2016.bin", "r")):
+    def __init__(self, path=("", "./data/wordcount.json")):
         self.TotalWords = 0
         self.Frequencies = {}
         self.fileSize = 0
@@ -22,6 +21,9 @@ class ZipfLaw:
         data = self.essay.read()
         wordList = data.split()
         self.Frequencies = Counter(wordList)
+        print("===========================================================================================")
+        print("||                                   1. THE ZIPF LAW                                     ||")
+        print("===========================================================================================")
         print(self.Frequencies)
         self.fileSize = data.__sizeof__()
         self.TotalWords = len(wordList)
@@ -31,29 +33,35 @@ class ZipfLaw:
 
     def draw(self, WordsLimit=150):
         probabilities = {}
-        Ci = {}
         self.Frequencies = {k: v for k, v in reversed(sorted(self.Frequencies.items(), key=lambda item: item[1]))}
 
-        for key in self.Frequencies.keys():
-            Ci.update({key: (self.Frequencies[key] / self.TotalWords)})
+        C = 0
+        for i in range(1, self.DictionaryVolume):
+            C += 1 / i
+        C = 1 / C
 
         count = 1
-        for key in Ci:
-            probabilities.update({key: Ci[key] / count})
+        for key in self.Frequencies.keys():
+            probabilities.update({key: C / count})
             count += 1
 
         sumPi = 0
         for key in probabilities.keys():
             sumPi += probabilities[key] * (-math.log10(probabilities[key]))
 
-        print("Probabilidades")
-        print(probabilities)
-        print("Cantidad total de palabras en el diccionario:")
+        print("Cantidad total de palabras en el texto:")
         print(self.TotalWords)
         print("Longitud inicial del texto")
         print(self.fileSize)
-        print("Volumen del diccionario del Lenguaje Natural")
+        print("Volumen del diccionario del Lenguaje Natural (Cantidad total de palabras diferentes)")
         print(self.DictionaryVolume)
+        print("Nueva Constante")
+        print(C)
+        print("Probabilidades")
+        print(probabilities)
+        print("===========================================================================================")
+        print("||          2.-CODING WORDS, UNIFORMLY AND NONUNIFORMLY WITH RESPECT TO OUTPUT           ||")
+        print("===========================================================================================")
         print("Bajo la codificacion de Shannon B1 (Longitud del texto comprimido) es igual a:")
         B1 = self.TotalWords * sumPi
         print(round(B1, 4), "bits")
@@ -61,33 +69,36 @@ class ZipfLaw:
         B2 = self.TotalWords * math.log10(self.DictionaryVolume)
         print(round(B2, 4), "bits")
         B1final = 0
-        # for key in self.Frequencies.keys():
-        #     sum = 0
-        #     sum2 = 0
-        #     sumD = 0
-        #     for i in range(1, self.DictionaryVolume):
-        #         sumD += 1 / i
-        #         sum += ((self.Frequencies[key] / i) * (math.log(i, 10) - math.log(self.Frequencies[key] + 1)))
-        #         sum2 += ((math.log(i) / i) + (math.log(2) - math.log(self.Frequencies[key]) * sumD))
-        #         if B1final < (self.TotalWords * sum):
-        #             B1final = ((self.TotalWords * self.Frequencies[key]) / math.log(2)) * sum2
-        #             if B1final < (self.TotalWords / math.log(2) * math.log(self.DictionaryVolume + 1)) * (
-        #                     (math.pow(math.log(self.DictionaryVolume + 1), 2)) + -0.105 + (
-        #                     math.log(2) + math.log(math.log(self.DictionaryVolume + 1)) + 0.577) * (
-        #                             math.log(self.DictionaryVolume + 1) + 0.577)) and (
-        #                     (self.TotalWords / math.log(2) * math.log(self.DictionaryVolume + 1)) *
-        #                     ((math.pow(math.log(self.DictionaryVolume + 1), 2) / 2)
-        #                      + (-0.105) +
-        #                      (math.log(2) + math.log(2) * math.log(self.DictionaryVolume + 1) + (
-        #                              0.577 / math.log(self.DictionaryVolume + 1))) * (
-        #                              math.log(self.DictionaryVolume + 1) + 0.577))):
-        #                 B1final = ((self.TotalWords / (math.log(2) * math.log(self.DictionaryVolume + 1))) * (
-        #                         (math.pow(math.log(self.DictionaryVolume + 1), 2)) / 2) + 0.577 * (1 + math.log(2)) + (
-        #                                -0.105) + (
-        #                                    math.log(2) * math.log(self.DictionaryVolume + 1)) * (
-        #                                    math.log(self.DictionaryVolume + 1) + math.log(2)) + 0.577 * (
-        #                                    math.log(2) * math.log(self.DictionaryVolume + 1)) + (
-        #                                    math.pow(0.577, 2) / math.log(self.DictionaryVolume + 1)))
+        for key in self.Frequencies.keys():
+            sum = 0
+            sum2 = 0
+            sumD = 0
+            for i in range(1, self.DictionaryVolume):
+                sumD += 1 / i
+                sum += ((self.Frequencies[key] / i) * (math.log(i, 10) - math.log(self.Frequencies[key] + 1)))
+                sum2 += ((math.log(i) / i) + (math.log(2) - math.log(self.Frequencies[key]) * sumD))
+                if B1final < (self.TotalWords * sum):
+                    B1final = ((self.TotalWords * self.Frequencies[key]) / math.log(2)) * sum2
+                    if B1final < (self.TotalWords / math.log(2) * math.log(self.DictionaryVolume + 1)) * (
+                            (math.pow(math.log(self.DictionaryVolume + 1), 2)) + -0.105 + (
+                            math.log(2) + math.log(math.log(self.DictionaryVolume + 1)) + 0.577) * (
+                                    math.log(self.DictionaryVolume + 1) + 0.577)) and (
+                            (self.TotalWords / math.log(2) * math.log(self.DictionaryVolume + 1)) *
+                            ((math.pow(math.log(self.DictionaryVolume + 1), 2) / 2)
+                             + (-0.105) +
+                             (math.log(2) + math.log(2) * math.log(self.DictionaryVolume + 1) + (
+                                     0.577 / math.log(self.DictionaryVolume + 1))) * (
+                                     math.log(self.DictionaryVolume + 1) + 0.577))):
+                        B1final = ((self.TotalWords / (math.log(2) * math.log(self.DictionaryVolume + 1))) * (
+                                (math.pow(math.log(self.DictionaryVolume + 1), 2)) / 2) + 0.577 * (1 + math.log(2)) + (
+                                       -0.105) + (
+                                           math.log(2) * math.log(self.DictionaryVolume + 1)) * (
+                                           math.log(self.DictionaryVolume + 1) + math.log(2)) + 0.577 * (
+                                           math.log(2) * math.log(self.DictionaryVolume + 1)) + (
+                                           math.pow(0.577, 2) / math.log(self.DictionaryVolume + 1)))
+
+        print("b1 final")
+        print(B1final)
 
         n1 = B1 / self.fileSize
         n2 = B2 / self.fileSize
@@ -99,8 +110,28 @@ class ZipfLaw:
         print(n1, "bits")
         print("n2")
         print(n2, "bits")
+        print("n1 (%)")
         print((B1 / self.fileSize) * 100)
+        print("n2 (%)")
         print((B2 / self.fileSize) * 100)
+
+        print("===========================================================================================")
+        print("||                           3. THE SINGLE-PASS COMPRESSION SCHEME                       ||")
+        print("===========================================================================================")
+
+        li = {}
+        count = self.DictionaryVolume
+        for key in self.Frequencies.keys():
+            li.update({key: -math.log10((C * self.DictionaryVolume) / count)})
+            count -= 1
+
+        Ksp = 0
+        for key in li:
+            Ksp *= li[key]
+
+        print(li)
+        print(Ksp)
+
 
 
 if __name__ == '__main__':
--- PIA/zipf.py
+++ PIA/zipf.py
@@ -3,7 +3,17 @@ import sys
 import src.visual as sv
 
 try:
-    sv.ZipfLaw()
+    sv.ZipfLaw(path=("dictionary1.txt", "./data/wordcount.json"))
+    print(
+        "------------------------------------------------------------------------------------------------------------")
+    sv.ZipfLaw(path=("oil_bourse.txt", "./data/wordcount.json"))
+
+    print("REFERENCES")
+    print("http://www.mathnet.ru/links/c6d04548ffd7ea92149c511a1190b1f5/da511.pdf")
+    print("http://www.mathnet.ru/php/archive.phtml?wshow=paper&jrnid=da&paperid=511&option_lang=eng")
+    print("http://www.mathnet.ru/links/4b1d0d48883ed22d8716aed768a81949/da_511_refs_eng.pdf")
+    print("(Application of the Zipf law to text compression) "
+          "https://link.springer.com/article/10.1134/S1990478908040042")
 except Exception as error:
     print(error)
 finally:
